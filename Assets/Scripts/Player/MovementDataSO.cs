9f714501f4cf2259937f8c8608a486df15d18b51
WindyFeg <phongspeed7@yahoo.com.vn>
2025-06-15 10:16:03 +0700

    Pick: "feat: add player basic movement"

---
+++ Assets/Scripts/Player/MovementDataSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MovementDataSO", menuName = "Scriptable Objects/MovementDataSO")]
+public class MovementDataSO : ScriptableObject
+{
+    public float moveSpeed = 5f;
+}

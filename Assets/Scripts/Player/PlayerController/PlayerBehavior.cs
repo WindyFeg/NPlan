cedcdb98c1fd699d721482b6deeae25dfee0fcb7
DuuuKieee <loi.tt@amanotes.com>
2025-06-18 21:18:33 +0700

    add player behavior

---
+++ Assets/Scripts/Player/PlayerController/PlayerBehavior.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace LKT268.Model.CommonBase
+{
+    public class PlayerBehavior : HumanBase
+    {
+        public PlayerBehavior(int id, string name, int maxHealth, int level, int damage) : base(id, name, maxHealth, level, damage)
+        {
+        }
+    }
+}
